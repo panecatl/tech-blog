bbc6b7118bb32ae302a0eaf51bccb6817fc201fa
panecatl <ypanecatl24@gmail.com>
2022-08-07 15:25:44 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,14 @@
-    // import the sequelize constructor from the library
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our database
+// create connection to our db
 const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
     });
 
 module.exports = sequelize;
